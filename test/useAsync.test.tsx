96897e9aad011cd76c034a68ec39a84ea584923b
Amine Ben hammou <webneat@gmail.com>
2019-10-11 05:56:03 +0100

    Add useAsync

--- test/useAsync.test.tsx
+++ test/useAsync.test.tsx
@@ -1,63 +1,123 @@
 import React, { Suspense } from 'react'
 import { act } from 'react-dom/test-utils'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { useAsync, createMemoryStorage } from '../src'
-
-const storage = createMemoryStorage()
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
-const Resolve = ({ data, ms }: any) => {
-  const [result] = useAsync(
-    'key',
-    async () => {
-      await delay(ms)
-      return data
-    },
-    storage
+const storage = createMemoryStorage()
+
+type AsyncProps = {
+  name: string
+  fn: () => Promise<string | number>
+}
+
+const Async = ({ name, fn }: AsyncProps) => {
+  const [data, rerun] = useAsync(name, fn, storage)
+  return (
+    <>
+      <p>{data}</p>
+      <button onClick={rerun}>Rerun</button>
+    </>
   )
-  return <p>{JSON.stringify(result)}</p>
+}
+
+const Example = ({ name, fn }: AsyncProps) => (
+  <Suspense fallback={<p>Loading...</p>}>
+    <Async {...{ name, fn }} />
+  </Suspense>
+)
+
+class ErrorBoundary extends React.Component {
+  componentDidCatch() {}
+  render() {
+    return <p>Error!</p>
+  }
 }
 
 describe('useAsync', () => {
-  afterEach(storage.clear)
   it('renders fallback while waiting for the promise', async () => {
-    const { container } = render(
-      <Suspense fallback={<p>Loading...</p>}>
-        <Resolve data="some data" ms={100} />
-      </Suspense>
-    )
+    const fn = async () => {
+      await delay(20)
+      return 'something'
+    }
+    const { container } = render(<Example name="renders-fallback" fn={fn} />)
     expect(container.innerHTML).toBe('<p>Loading...</p>')
   })
 
   it('renders the result when the promise resolves', async () => {
+    const fn = async () => {
+      await delay(20)
+      return 'something'
+    }
     let container: any = null
     await act(async () => {
-      container = render(
-        <Suspense fallback={<p>Loading...</p>}>
-          <Resolve data="some data" ms={100} />
-        </Suspense>
-      ).container
-      await delay(100)
+      container = render(<Example name="renders-result" fn={fn} />).container
+      expect(container.innerHTML).toBe('<p>Loading...</p>')
+      await delay(20)
     })
-    expect(container.innerHTML).toBe('<p>"some data"</p>')
+    expect(container.innerHTML).toContain('<p>something</p>')
   })
 
   it('caches the result for next renders', async () => {
+    let count = 0
+    const fn = async () => {
+      count++
+      await delay(20)
+      return 'something'
+    }
+    let result: any = null
+    await act(async () => {
+      result = render(<Example name="caches-result" fn={fn} />)
+      expect(result.container.innerHTML).toBe('<p>Loading...</p>')
+      await delay(20)
+    })
+    expect(result.container.innerHTML).toContain('<p>something</p>')
+    expect(count).toBe(1)
+    result.rerender(<Example name="caches-result" fn={fn} />)
+    expect(result.container.innerHTML).toContain('<p>something</p>')
+    expect(count).toBe(1)
+  })
+
+  it('reruns the async function', async () => {
+    let count = 0
+    const fn = async () => {
+      count++
+      await delay(20)
+      return 'something'
+    }
+    let result: any = null
+    await act(async () => {
+      result = render(<Example name="reruns-function" fn={fn} />)
+      expect(result.container.innerHTML).toBe('<p>Loading...</p>')
+      await delay(20)
+    })
+    expect(result.container.innerHTML).toContain('<p>something</p>')
+    expect(count).toBe(1)
+    await act(async () => {
+      fireEvent.click(result.getByText('Rerun'))
+      await delay(5)
+      expect(result.container.innerHTML).toContain('<p>Loading...</p>')
+      await delay(20)
+    })
+    expect(result.container.innerHTML).toContain('<p style="">something</p>')
+    expect(count).toBe(2)
+  })
+
+  it('throws when the function fails', async () => {
+    const fn = async () => {
+      await delay(20)
+      throw 'Error'
+    }
     let result: any = null
     await act(async () => {
       result = render(
-        <Suspense fallback={<p>Loading...</p>}>
-          <Resolve data="some data" ms={100} />
-        </Suspense>
+        <ErrorBoundary>
+          <Example name="throws-error" fn={fn} />
+        </ErrorBoundary>
       )
-      await delay(100)
+      await delay(20)
     })
-    result.rerender(
-      <Suspense fallback={<p>Loading...</p>}>
-        <Resolve data="some data" ms={100} />
-      </Suspense>
-    )
-    expect(result.container.innerHTML).toBe('<p>"some data"</p>')
+    expect(result.container.innerHTML).toContain('<p>Error!</p>')
   })
 })
