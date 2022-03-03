01eef2621b82cf5271f43fff5b778833cb89a23c
Amine Ben hammou <webneat@gmail.com>
2022-03-03 01:23:58 +0100

    Use default import for React

--- src/useStorage/useStorage.ts
+++ src/useStorage/useStorage.ts
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react'
+import React from 'react'
 import {isFunction, isInBrowser} from '../utils'
 import {getDefaultStorage} from './defaultStorage'
 import {createMemoryStorage} from './createMemoryStorage'
@@ -8,12 +8,12 @@ type Value<T> = T | null | ((x: T | null) => T | null)
 
 export function useStorage<T>(key: string, defaultValue: T | null = null, getStorage = getDefaultStorage()) {
   const isMounted = useIsMounted()
-  const storage = useMemo<Storage>(() => {
+  const storage = React.useMemo<Storage>(() => {
     if (!isInBrowser()) return createMemoryStorage()
     if (isFunction(getStorage)) return (getStorage as any)()
     return getStorage
   }, [getStorage])
-  const initialValue = useMemo(() => {
+  const initialValue = React.useMemo(() => {
     const storedString = storage.getItem(key)
     return storedString === null ? defaultValue : JSON.parse(storedString)
   }, [storage, key, defaultValue])
